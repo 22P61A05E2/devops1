acaf466bafb08a878b5da85fb343648e04b0cd02
22P61A05E2 <22p61a05e2@vbithyd.ac.in>
2025-03-08 07:11:39 +0530

    ..

--- hello.py
+++ hello.py
@@ -1,9 +1,4 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-# Calculating sum
+num1 = 10  # Example value
+num2 = 20  # Example value
 sum_result = num1 + num2
-
-# Displaying result
 print("The sum is:", sum_result)
-
