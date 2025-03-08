583f79080590fb5ac235b40629e6b6fc8c814aa9
22P61A05E2 <22p61a05e2@vbithyd.ac.in>
2025-03-08 07:08:20 +0530

    changes made

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
-print(2+3)
-print("HEllo")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculating sum
+sum_result = num1 + num2
+
+# Displaying result
+print("The sum is:", sum_result)
 
