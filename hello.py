7180ce73133c9cd1764a9eaa0275ccfe5d710b9f
22P61A05E2 <22p61a05e2@vbithyd.ac.in>
2025-03-08 07:17:22 +0530

    updated

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
-num1 = 10  # Example value
-num2 = 20  # Example value
+num1 = 10  
+num2 = 20  
 sum_result = num1 + num2
 print("The sum is:", sum_result)
+
+num = 5  
+factorial = 1
+for i in range(1, num + 1):
+    factorial *= i
+print(f"Factorial of {num} is {factorial}")
+
+text = "Hello, World!"  
+reversed_text = text[::-1]
+print("Reversed string:", reversed_text)
